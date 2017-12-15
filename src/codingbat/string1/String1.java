c5c52cb3a216bb668d5cecfa77b5a8368492e1dc
sh0stik <sh0stik@mail.ru>
2017-12-15 16:54:04 +0300

    Homework Hackerrank

--- src/codingbat/string1/String1.java
+++ src/codingbat/string1/String1.java
@@ -119,15 +119,73 @@ public class String1 {
     }
 
     public boolean hasBad(String str) {
-        int length = str.length();
+        if (str.length() == 3 && str.equals("bad")) return true;
         if (str.length() < 3 || (str.substring(1).length() < 3 && !(str.substring(1).equals("bad")))) return false;
         if ((str.substring(0, 3).equals("bad") || (str.substring(1, 4).equals("bad")))) {
             return true;
         }
-
         return false;
+    }
+
+    public String atFirst(String str) {
+        if (str.length() < 1) return "@@";
+        if (str.length() < 2) return str + "@";
+        return str.substring(0, 2);
 
     }
+
+    public String lastChars(String a, String b) {
+        if (a.length() < 1 && b.length() < 1) return "@@";
+        if (a.length() < 1) return "@" + b.substring(b.length() - 1);
+        if (b.length() < 1) return a.substring(0, 1) + "@";
+        return a.substring(0, 1) + b.substring(b.length() - 1);
+    }
+
+    public String conCat(String a, String b) {
+        if (a.length() == 0 || b.length() == 0) return a + b;
+        if (a.charAt(a.length() - 1) == b.charAt(0)) return (b.length() != 1) ? a + b.substring(1, b.length()) : a;
+        return a + b;
+    }
+
+    public String lastTwo(String str) {
+        if (str.length() == 0) return "";
+        if (str.length() < 3) return str.substring(1) + str.substring(0, 1);
+        return str.substring(0, str.length() - 2) + str.charAt(str.length() - 1) + str.charAt(str.length() - 2);
+    }
+
+    public String seeColor(String str) {
+
+        if (str.length() >= 3 && str.substring(0, 3).equals("red")) return str.substring(0, 3);
+        if (str.length() >= 4 && str.substring(0, 4).equals("blue")) return str.substring(0, 4);
+        else return "";
+    }
+
+    public boolean frontAgain(String str) {
+        if (str.length() <= 1) return false;
+        if (str.length() < 4 && str.length() > 1) return true;
+        if (str.substring(0, 2).equals(str.substring(str.length() - 2))) return true;
+        return false;
+    }
+
+    public String minCat(String a, String b) {
+        if (a.length() > b.length()) return a.substring(a.length() - b.length()) + b;
+        else return a + b.substring(b.length() - a.length());
+    }
+
+    public String extraFront(String str) {
+        if (str.length() == 0) return "";
+        if (str.length() <= 2) return str + str + str;
+        return str.substring(0, 2) + str.substring(0, 2) + str.substring(0, 2);
+    }
+
+    public String without2(String str) {
+        if (str.length() < 1) return "";
+        if (str.length() == 1) return str;
+        if ( str.substring(0,2).equals(str.substring(str.length()-2))) return str.substring(2);
+        return str;
+    }
+
+    
 
     public static void main(String[] args) {
         String1 par = new String1();
