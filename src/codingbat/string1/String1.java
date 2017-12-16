f556e6853d3e90e241278dc46d56cf2aa593755d
sh0stik <sh0stik@mail.ru>
2017-12-16 15:26:55 +0300

    Homework Codingbat String 1

--- src/codingbat/string1/String1.java
+++ src/codingbat/string1/String1.java
@@ -181,11 +181,50 @@ public class String1 {
     public String without2(String str) {
         if (str.length() < 1) return "";
         if (str.length() == 1) return str;
-        if ( str.substring(0,2).equals(str.substring(str.length()-2))) return str.substring(2);
+        if (str.substring(0, 2).equals(str.substring(str.length() - 2))) return str.substring(2);
         return str;
     }
 
-    
+    public String deFront(String str) {
+        if (str.charAt(0) == 'a' && str.charAt(1) == 'b') return str;
+        if (str.charAt(0) == 'a' && str.charAt(1) != 'b') return str.charAt(0) + str.substring(2);
+        if (str.charAt(0) != 'a' && str.charAt(1) == 'b') return str.substring(1);
+        return str.substring(2);
+    }
+
+    public String startWord(String str, String word) {
+        String temp;
+        if (str.length() >= word.length()) {
+            temp = str.substring(1, word.length());
+            if (word.substring(1).equals(temp)) return str.charAt(0) + temp;
+            else return "";
+        } else return "";
+    }
+
+    public String withoutX(String str) {
+        if (str.length() <= 1 && str.charAt(0) == 'x') return "";
+        if (str.charAt(0) == 'x' && str.charAt(str.length() - 1) == 'x')
+            return str.substring(1, str.length() - 1);
+        if (str.charAt(0) == 'x' && str.charAt(str.length() - 1) != 'x')
+            return str.substring(1);
+        if (str.charAt(0) != 'x' && str.charAt(str.length() - 1) == 'x')
+            return str.substring(0, str.length() - 1);
+        else return str;
+    }
+
+    public String withoutX2(String str) {
+        if ((str.length() <= 1) || (str.length() == 2 && str.charAt(0) == 'x' && str.charAt(1) == 'x'))
+            return "";
+        if (str.charAt(0) == 'x' && str.charAt(1) == 'x')
+            return str.substring(2);
+        if (str.charAt(0) == 'x' && str.charAt(1) != 'x')
+            return str.substring(1);
+        if (str.charAt(0) != 'x' && str.charAt(1) == 'x')
+            return str.charAt(0)+str.substring(2);
+        else return str;
+
+    }
+
 
     public static void main(String[] args) {
         String1 par = new String1();
