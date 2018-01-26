ffd387e69a61ba4fb054ea05dc642b0db1fb27c7
sh0stik <sh0stik@mail.ru>
2018-01-26 15:44:00 +0300

    adding some challenges from Hackerrank

--- src/main/java/hackerrank/dataStructures/JavaGenerics.java
+++ src/main/java/hackerrank/dataStructures/JavaGenerics.java
@@ -1,12 +1,36 @@
 package hackerrank.dataStructures;
 
 import java.util.Scanner;
+import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class JavaGenerics {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
 
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
 
     }
+
+
 }
 
+class Printer{
+    public <E> void printArray(E[] arr){
+        for (E anArr : arr) {
+            System.out.println(anArr);
+        }
+    }
+}
